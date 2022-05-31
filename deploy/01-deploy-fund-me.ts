f7a5e86c77b98e32d20383f899e7fb2a4b5cf60f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-05-31 09:46:31 -0300

    add hardhat-deploy and contracts

---
+++ deploy/01-deploy-fund-me.ts
@@ -0,0 +1,11 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deployFundMe: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  console.log(network.config.chainId);
+};
+export default deployFundMe;
