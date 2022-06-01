acf27244ce4e719b291620a9c2ccf48ef27ac829
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-01 09:52:06 -0300

    add get storage functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -60,6 +60,30 @@ contract FundMe {
         }("");
         require(callSuccess, "Call failed");
     }
+
+    function getAddressToAmountFunded(address fundingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return s_priceFeed;
+    }
 }
 
 // Explainer from: https://solidity-by-example.org/fallback/
